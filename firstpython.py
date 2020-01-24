255b09eb027bb80be4a89fc5d4c13576b377f0d9
Administrator <pratimmallick@gmail.com>
2020-01-24 19:02:25 +0530

    File modified

--- firstpython.py
+++ firstpython.py
@@ -1,10 +1,17 @@
 test = " welcome"
-
 print("Hello Python"+ test)
 
 """""
 This is First Python Program 
 """""
+name = " Binod "
+age  = 20
+cgpa = 4.1
+
+print( name + " is ", age , " years old and his cgpa", cgpa   )
+
+
+
 
 
 
